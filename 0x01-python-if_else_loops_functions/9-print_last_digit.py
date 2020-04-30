8761c7117469bbd2490ca5d2bdb326af11af1b8a
monicajoa <mnortiz.ortiz@gmail.com>
2020-04-30 19:22:24 +0000

    0x01. Python - if/else, loops, functions, 9. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important, check the indentation

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-        if number < 0:
-	        number = number * -1
-        lastdigit = number % 10 
-        print("{:d}".format(lastdigit), end='')
-        return (lastdigit)
+    if number < 0:
+        number = number * -1
+        ld = number % 10
+        print("{:d}".format(ld), end='')
+        return (ld)

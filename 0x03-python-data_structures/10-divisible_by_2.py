3d2bcfc478a7c2ebc8c848d2a216ce2f2c380504
monicajoa <mnortiz.ortiz@gmail.com>
2020-05-06 05:02:05 +0000

    0x03. Python - Data Structures: Lists, Tuples, 10. Only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    list_new = []
+        for integer in my_list:
+            if integer % 2 == 0:
+                list_new.append(True)
+            else:
+                list_new.append(False)
+        return (list_new)

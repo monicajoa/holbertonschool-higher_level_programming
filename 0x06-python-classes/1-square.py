c874ba4a8259fe0a97c6c188a3b89a935d010254
monicajoa <mnortiz.ortiz@gmail.com>
2020-05-19 19:47:00 -0500

    0x06. Python - Classes and Objects, 1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""1- Square with zise"""
+
+
+class Square:
+    """Class Square that defines a square.
+
+    attributes:
+        size: The size of a square.
+    """
+
+    # method to initialize an object or instance
+    def __init__(self, size):
+        self.__size = size

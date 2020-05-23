fb065d2c8e9e47ec154480e067a27ef3e40152db
monicajoa <mnortiz.ortiz@gmail.com>
2020-05-22 19:53:39 -0500

    0x07. Python - Test-driven development, readme, task_0 and doctest

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module hold a function to add 2 numbers those must
+be integers or floats
+"""
+
+
+def add_integer(a, b=98):
+    """
+    This function adds 2 integers or floats
+    Paramethers:
+        a (float, int): The first value
+        b (float, int): The second value. Defaults to 98.
+    Errors:
+        TypeError: if a is not a float or an int
+        TypeError: if b is not a float or an int
+    Returns:
+        Integer with the addition of a and b
+    """
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    elif type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    else:
+        if type(a) == float:
+            a = int(a)
+        if type(b) == float:
+            b = int(b)
+        return (a + b)

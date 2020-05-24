a1bbcbe1304b51822c08e07b87e0731229c69f6c
monicajoa <mnortiz.ortiz@gmail.com>
2020-05-24 03:16:55 -0500

    0x07. Python - Test-driven development, 2. Say my name, add main and file with the task_2

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module hold a function that prints My name is <first name> <last name>.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    This function prints the first and the last name
+    Paramethers:
+        first_name:the first name
+        last_Name: The last name
+    Errors:
+        TypeError: first_name must be a string
+        TypeError: last_name must be a string
+    Returns:
+        Nothing
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))

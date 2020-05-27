e5d99d3ff8cbd0ab12336c58fdefdbbd9664e321
monicajoa <mnortiz.ortiz@gmail.com>
2020-05-26 21:39:53 -0500

    0x09. Python - Everything is object, add files, task 31. Low memory cost, avanced,

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class LockedClass with no class or object attribute
+"""
+
+
+class LockedClass:
+    """prevents the user from dynamically
+    creating new instance attributes
+    """
+    __slots__ = ["first_name"]

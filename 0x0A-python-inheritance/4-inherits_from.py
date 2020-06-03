04e165f5e2430d7be928e5a50ee6c3aeb19a6f63
monicajoa <mnortiz.ortiz@gmail.com>
2020-06-02 21:48:30 -0500

    0x0A. Python - Inheritance, 4. Only sub class of, add file with documentation and main, check style pep8

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,8 +5,8 @@
 
 
 def inherits_from(obj, a_class):
-    """function that returns True if the object is an instance of a class that 
-    inherited (directly or indirectly) from the specified class ; otherwise False.
+    """function that returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class.
 
     Arguments:
         obj {[object]} -- object to check

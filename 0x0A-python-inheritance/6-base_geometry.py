e9affff5b6e3532309e16eeacaba01c50654aa0a
monicajoa <mnortiz.ortiz@gmail.com>
2020-06-02 22:06:24 -0500

    0x0A. Python - Inheritance, 6. Improve Geometry, add file and main

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module holds a function with
+Geometry module
+    """
+
+
+class BaseGeometry:
+    """Improve Geometry
+    """
+
+    def area(self):
+        """Public instance method that raise an exception
+
+        Raises:
+            Exception: area() is not implemented
+        """
+        raise Exception("area() is not implemented")

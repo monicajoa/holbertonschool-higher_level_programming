d7448c9b01c30987a8a0efd010220cbd06fe603f
monicajoa <mnortiz.ortiz@gmail.com>
2020-06-02 21:28:49 -0500

    0x0A. Python - Inheritance, 3. Same class or inherit from, add file with documentation

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
+"""This module holds a function that
+    check if is Same class or inherit from
+    """
+
+
 def is_kind_of_class(obj, a_class):
+    """function that returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class ; otherwise False
+
+    Arguments:
+        obj {[object]} -- object to check
+        a_class {[class]} -- specified class
+
+    Returns:
+        [bool] -- if is an instance or inherited return true otherwise false
+    """
     return isinstance(obj, a_class)

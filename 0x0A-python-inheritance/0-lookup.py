3e9a3b5bf93794bfde882061f50fd679e0621069
monicajoa <mnortiz.ortiz@gmail.com>
2020-06-02 19:03:30 -0500

    0x0A. Python - Inheritance, 0. Lookup, task, add file, add identation and documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
+    """This module holds a function that returns the list 
+    of available attributes and methods of an object
+    """
+
+
 def lookup(obj):
+    """function that returns the list 
+    of available attributes and methods of an object
+
+    Arguments:
+        obj {[object]} -- object to check
+
+    Returns:
+        [list] -- available attributes and methods of an object
+    """
     return dir(obj)

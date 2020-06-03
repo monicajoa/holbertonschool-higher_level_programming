251e42074d4205b40b02c2a392c5aeb84d85648d
monicajoa <mnortiz.ortiz@gmail.com>
2020-06-02 23:35:07 -0500

    0x0A. Python - Inheritance, 9. Full rectangle, add file and main

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This module holds a class Rectangle
+    """
+BaseGeo = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeo):
+    """class rectangle that inherits from BaseGeometry
+
+    Arguments:
+        BaseGeo {[class]} -- Baseclass
+    """
+
+    def __init__(self, width, height):
+        """Constructor or initializer
+
+        Arguments:
+            width {[int]} -- width of the rectangle
+            height {[int]} -- height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """area of the rectangle
+
+        Returns:
+            [int] -- the area of the rectangle
+        """
+        return self.__height * self.__width
+
+    def __str__(self):
+        """printable string
+
+        Returns:
+            [str] -- rectangle representation
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)

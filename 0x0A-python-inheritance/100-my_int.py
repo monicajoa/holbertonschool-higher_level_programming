3395471a660360c3b500eeaa427734d094153a18
monicajoa <mnortiz.ortiz@gmail.com>
2020-06-03 00:50:55 -0500

    0x0A. Python - Inheritance, advances, 12. My integer, add file and main

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""This module holds a class MyInt
+    """
+
+
+class MyInt(int):
+    """new class MyInt has == and != operators inverted
+
+    Arguments:
+        int {[class]} -- Parent class
+    """
+
+    def __eq__(self, number):
+        """equal method
+
+        Arguments:
+            number {[int]} -- integer to compare
+
+        Returns:
+            [bool] -- inverted operator
+        """
+        return int(self) != int(number)
+
+    def __ne__(self, number):
+        """diferent method
+
+        Arguments:
+            number {[int]} -- integer to compare
+
+        Returns:
+            [bool] -- inverted operator
+        """
+        return int(self) == int(number)

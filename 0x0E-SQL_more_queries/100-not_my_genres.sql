00ad5605a2364d1ea2b845254828ee4f335c6c79
monicajoa <mnortiz.ortiz@gmail.com>
2020-07-14 23:49:04 -0500

    SQL - More queries, task avanced 17. Not my genre, add 100-not_my_genres.sql file, script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter, change code and syntax

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,11 +1,11 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server: (same as 16-shows_by_genre.sql)
 -- Script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
-SELECT name
+SELECT tv_genres.name
 FROM tv_genres
-WHERE name NOT IN (
-SELECT tg.name
-FROM tv_shows ts
-JOIN tv_show_genres tsg ON ts.id = tsg.show_id
-JOIN tv_genres tg ON tsg.genre_id = tg.id
-WHERE ts.title = 'Dexter')
-ORDER BY name;
+WHERE name not in (
+    SELECT tv_genres.name
+    FROM tv_show_genres
+    INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_shows.title = "Dexter")
+ORDER BY tv_genres.name ASC;

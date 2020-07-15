e081399e5d9629e23e1534d7b1f9927d810dee64
monicajoa <mnortiz.ortiz@gmail.com>
2020-07-14 21:17:42 -0500

    SQL - More queries, task mandatory 7. Cities table, add 7-cities.sql filescript that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server, delete a whitespace

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,5 @@
 -- Script that creates the database hbtn_0d_usa and the table cities
---(in the database hbtn_0d_usa) on your MySQL server
+-- (in the database hbtn_0d_usa) on your MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (

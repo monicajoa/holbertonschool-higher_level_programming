669f1cb6c4cd0cd490b38242625e158d9d60e2e8
monicajoa <mnortiz.ortiz@gmail.com>
2020-07-14 23:20:07 -0500

    SQL - More queries, task mandatoty 16. List shows and genres , add 16-shows_by_genre.sql file, script that lists all shows, and all genres linked to that show, from the database, add left join

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,8 +2,8 @@
 -- Script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
 SELECT tv_shows.title, tv_genres.name
 FROM tv_show_genres
-JOIN tv_genres
+LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-JOIN tv_shows
+LEFT JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 ORDER BY tv_shows.title, tv_genres.name ASC;

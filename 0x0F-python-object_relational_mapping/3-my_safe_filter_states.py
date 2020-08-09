7edf2eaede84bb4db41e17775479fc20eb7c4564
monicajoa <mnortiz.ortiz@gmail.com>
2020-08-09 01:13:13 -0500

    0x0F. Python - Object-relational mapping project, mandatory task 3. SQL Injection, add 3-my_safe_filter_states.py file,  script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But this time, write one that is safe from MySQL injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" SQL Injection
+    script that takes in arguments and displays all values
+    in the states table of hbtn_0e_0_usa, but safe from MySQL injections
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY %s", (argv[4], ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)

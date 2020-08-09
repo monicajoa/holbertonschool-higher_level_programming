c33be0693f9a2d4ae30d0257da3b32941446c15d
monicajoa <mnortiz.ortiz@gmail.com>
2020-08-09 00:28:21 -0500

    0x0F. Python - Object-relational mapping project, mandatory task 2. Filter states by user input, add 2-my_filter_states.py file,  script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Filter states by user input
+    script that takes in an argument and displays all values
+    in the states table of hbtn_0e_0_usa where name matches the argument
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)

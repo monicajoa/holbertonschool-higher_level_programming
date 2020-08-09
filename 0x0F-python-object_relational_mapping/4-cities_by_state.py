601a39554bb39347b094d34e10ffb7678e37d1d8
monicajoa <mnortiz.ortiz@gmail.com>
2020-08-09 02:45:19 -0500

    0x0F. Python - Object-relational mapping project, mandatory task 4. Cities by states, add 4-cities_by_state.py file and other files with the close db and close cur

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-""" Filter states by user input
-    script that takes in an argument and displays all values
-    in the states table of hbtn_0e_0_usa where name matches the argument
+""" Cities by states
+    script that lists all cities from the database hbtn_0e_4_usa
 """
 import MySQLdb
 from sys import argv
@@ -10,8 +9,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4]))
+    new_query = "SELECT cities.id, cities.name, states.name"
+    new_query += " FROM cities LEFT JOIN states ON cities.state_id=states.id"
+    cur.execute(new_query)
     rows = cur.fetchall()
     for row in rows:
         print(row)

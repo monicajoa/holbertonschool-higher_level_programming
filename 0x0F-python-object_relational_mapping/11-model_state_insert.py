6792197a733ace88fdee5c486fc21d3fc0599961
monicajoa <mnortiz.ortiz@gmail.com>
2020-08-09 21:09:06 -0500

    0x0F. Python - Object-relational mapping project, mandatory task 11. Add a new state, add 11-model_state_insert.py file, script that adds the State object “Louisiana” to the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Add a new state
+    script that adds the State object
+    “Louisiana” to the database hbtn_0e_6_usa
+"""
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}" .format(state.id, state.name))

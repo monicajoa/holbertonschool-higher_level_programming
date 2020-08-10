92bd0784d93fce360f5a986f96dcf6941c19c455
monicajoa <mnortiz.ortiz@gmail.com>
2020-08-09 21:20:23 -0500

    0x0F. Python - Object-relational mapping project, mandatory task 11. Add a new state, add 11-model_state_insert.py file, script that adds the State object “Louisiana” to the database hbtn_0e_6_usa, add session commit and session close

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -18,5 +18,7 @@ if __name__ == "__main__":
     session = Session()
     new_state = State(name='Louisiana')
     session.add(new_state)
+    session.commit()
     for state in session.query(State).order_by(State.id).all():
         print("{}: {}" .format(state.id, state.name))
+    session.close()

031140dd62b579fe6b75685f70b3d0b4e2ca1178
monicajoa <mnortiz.ortiz@gmail.com>
2020-08-30 23:16:26 -0500

    Python - Network #1 project, mandatory task 7. Error code #1, add 7-error_code.py file,  Python script that takes in a URL, sends a request to the URL and displays the body of the response, using the packages requests and sys, add text in the print

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
-        print(response)
+        print(response.text)

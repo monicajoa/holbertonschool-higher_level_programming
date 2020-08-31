11a2c6b25516415fcef1508fb19f3430c545d864
monicajoa <mnortiz.ortiz@gmail.com>
2020-08-31 01:16:45 -0500

    Python - Network #1 project, mandatory task 8. Search API, add 8-json_api.py file, Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter, using the package requests and sys

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Search API
+    This module holds a Python script that takes in a letter
+    and sends a POST request to http://0.0.0.0:5000/search_user
+    with the letter as a parameter, using  the package requests and sys
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(argv) > 1:
+        values = {'q': argv[1]}
+    else:
+        values = {'q': ""}
+    response = requests.post(url, data=values)
+    try:
+        json_r = response.json()
+        if json_r:
+            print("[{}] {}".format(json_r['id'], json_r['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")

17b13f2bb63d38d81a807b5586bd5464e13e7fa9
monicajoa <mnortiz.ortiz@gmail.com>
2020-08-30 19:35:32 -0500

    Python - Network #1 project, mandatory task Error code #0, add 3-error_code.py file, Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8), FIX THE PACKAGAGE and replace URLError to HTTPError

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,14 +4,13 @@
     sends a request to the URL and displays the body
     of the response (decoded in utf-8)
 """
-import urllib.request
-import urllib.error
+from urllib import request, error
 import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
     try:
-        with urllib.request.urlopen(url) as response:
+        with request.urlopen(url) as response:
             print(response.read().decode("utf-8"))
-    except urllib.error.URLError as e:
-        print("Error code:{} ".format(e.code))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))

986c5cc566d484b02b065f283ef91ef7a0660a79
monicajoa <mnortiz.ortiz@gmail.com>
2020-08-31 01:19:56 -0500

    Python - Network #1 project, mandatory task 8. Search API, add 8-json_api.py file, Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter, using the package requests and sys, fix the package sys.argv

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,7 +10,7 @@ import requests
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
     if len(argv) > 1:
-        values = {'q': argv[1]}
+        values = {'q': sys.argv[1]}
     else:
         values = {'q': ""}
     response = requests.post(url, data=values)
